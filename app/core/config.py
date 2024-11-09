c91efba8e981902d04aaa6fcbef0fc9264ddd101
23tae <taehoonkim.dev@gmail.com>
2024-11-10 03:10:27 +0900

    feat: SQLite에서 RDS PostgreSQL로 변경

--- app/core/config.py
+++ app/core/config.py
@@ -6,13 +6,21 @@ load_dotenv()
 
 
 class Settings(BaseSettings):
-    DATABASE_URL: str = os.getenv("DATABASE_URL")
     SECRET_KEY: str = os.getenv("SECRET_KEY")
     ALGORITHM: str = os.getenv("ALGORITHM")
     ACCESS_TOKEN_EXPIRE_MINUTES: int = os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES")
     # UPLOAD_DIR: str = os.getenv("UPLOAD_DIR", "images/")
     S3_BUCKET: str = os.getenv("S3_BUCKET")
     S3_REGION: str = os.getenv("S3_REGION")
+    DATABASE_HOST: str = os.getenv("DATABASE_HOST")
+    DATABASE_PORT: str = os.getenv("DATABASE_PORT")
+    DATABASE_NAME: str = os.getenv("DATABASE_NAME")
+    DATABASE_USERNAME: str = os.getenv("DATABASE_USERNAME")
+    DATABASE_PASSWORD: str = os.getenv("DATABASE_PASSWORD")
+    DATABASE_URL: str = (
+        f"postgresql://{DATABASE_USERNAME}:{DATABASE_PASSWORD}@{DATABASE_HOST}:{DATABASE_PORT}/{DATABASE_NAME}"
+    )
+    # DATABASE_URL: str = f"sqlite:///./ {settings.DATABASE_NAME}"
 
     class Config:
         env_file = ".env"
--- app/db.py
+++ app/db.py
@@ -3,15 +3,12 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 from .core.config import settings
 
-engine = create_engine(settings.DATABASE_URL, connect_args={"check_same_thread": False})
+
+engine = create_engine(settings.DATABASE_URL)
+# engine = create_engine(settings.DATABASE_URL, connect_args={"check_same_thread": False}) # SQLite
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
-
-
-# 데이터베이스 테이블을 초기화
-def init_database():
-    Base.metadata.create_all(bind=engine)
 
 
 # 데이터베이스 세션 의존성 제공
@@ -21,6 +18,11 @@ def get_db():
         yield db
     finally:
         db.close()
+
+
+# 데이터베이스 테이블을 초기화
+def init_database():
+    Base.metadata.create_all(bind=engine)
 
 
 # 개발 환경에서 데이터베이스 초기화
